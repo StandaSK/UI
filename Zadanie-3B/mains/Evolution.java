d2468e4ca5759a312136ad12a986447b79bb76bc
StandaSK <xjakubeks@stuba.sk>
2016-04-29 11:29:19 +0200

    Added files via upload

---
+++ Zadanie-3B/mains/Evolution.java
@@ -0,0 +1,86 @@
+package mains;
+
+import java.util.*;
+
+public class Evolution {
+	private static Set<String> allSteps = new HashSet<String>();
+	StringBuilder sb = new StringBuilder();
+	private int operationCounter;
+	private int newStepIndex;
+	private int[] newSteps;
+	
+	/**
+	 * Úvodná inicializácia prvých STARTING_MEMORY_CELL_COUNT krokov jedinca
+	 * @return Novo vygenerovaný jedinec
+	 */
+	public static StepSequence generate() {
+		int[] steps = new int[Main.MEMORY_CELL_COUNT];
+		
+		String path = "";
+		
+		while (allSteps.contains(path)) {
+			path = "";
+			for (int i = 0; i < Main.STARTING_MEMORY_CELL_COUNT; i++) {
+				steps[i] = (int) Math.floor(Math.random() * Main.MAX_VALUES);
+				path += steps[i];
+			}
+		}
+		allSteps.add(path);
+		return new StepSequence(steps);
+	}
+	
+	public void create(StepSequence ss) {
+		operationCounter = 0;
+		newStepIndex = 0;
+		
+		while (sb.length() < 49 && newStepIndex < Main.MEMORY_CELL_COUNT && operationCounter < Main.MAX_STEP_COUNT) {
+			operate();
+		}
+		
+		allSteps.add(sb.toString());
+		ss.setPath(sb.toString());
+	}
+	
+	private void operate() {
+		operationCounter++;
+		String operator = Main.getBytesOfInt(newSteps[newStepIndex]).substring(0, 2);
+		int temp;
+		
+		switch (operator) {
+			case "00":
+				temp = Main.getIntValue(Main.getBytesOfInt(newSteps[newStepIndex++]).substring(2, 8));
+				newSteps[temp] += 1;
+				break;
+			case "01":
+				temp = Main.getIntValue(Main.getBytesOfInt(newSteps[newStepIndex++]).substring(2, 8));
+				newSteps[temp] -= 1;
+				break;
+			case "10":
+				temp = Main.getIntValue(Main.getBytesOfInt(newSteps[newStepIndex++]).substring(2, 8));
+				newStepIndex = temp;
+				break;
+			case "11":
+				temp = Main.getIntValue(Main.getBytesOfInt(newSteps[newStepIndex++]).substring(2, 8));
+				sb.append(getLetter(newSteps[temp]));
+				break;
+		}
+	}
+	
+	/* Zo vstupu zisti kroky */
+	private char getLetter(int tmp){
+		byte[] num = Main.getBytesOfInt(tmp).getBytes();
+		int count = 0;
+		for (int i = 0; i < num.length; i++)
+			if (num[i] == 49)
+				count++;
+		
+		if (count <= 2)
+			return 'h';
+		if (count <= 4)
+			return 'd';
+		if (count <= 6)
+			return 'p';
+		
+		return 'l';
+	}
+}
---
+++ Zadanie-3B/mains/StepSequence.java
@@ -0,0 +1,54 @@
+package mains;
+
+public class StepSequence {
+	private int treasureCount;
+	private int stepCount;
+	private int[] steps;
+	
+	private float fitness;
+	
+	private String path;
+	
+	private boolean tracked;
+	private boolean exists;
+	
+	/* Konštruktor */
+	public StepSequence(int[] steps) {
+		this.setSteps(steps);
+	}
+	
+	/* Slúi na vıpis atribútov jedinca */
+	public String toString() {
+		return ("Krokov: " + stepCount + '\n' +
+				"Pokladov: " + treasureCount + '\n' +
+				"Fitness: " + fitness + '\n');
+	}
+	
+	/* Nastaví hodnoty a oznaèí sa ako u prejdenı */
+	public void track(int treasureCount, int stepCount) {
+		this.setTreasureCount(treasureCount);
+		this.setStepCount(stepCount);
+		this.setTracked(true);
+		this.setFitness(treasureCount - (stepCount / 1000));
+	}
+	
+	/* Gettery */
+	public int getTreasureCount() { return treasureCount; }
+	public int getStepCount() { return stepCount; }
+	public int[] getSteps() { return steps; }
+	public float getFitness() { return fitness; }
+	public String getPath() { return path; }
+	public boolean isTracked() { return tracked; }
+	public boolean isExists() { return exists; }
+	/* Settery */
+	public void setTreasureCount(int treasureCount) { this.treasureCount = treasureCount; }
+	public void setStepCount(int stepCount) { this.stepCount = stepCount; }
+	public void setSteps(int[] steps) { this.steps = steps; }
+	public void setFitness(float fitness) { this.fitness = fitness; }
+	public void setPath(String path) {
+		this.path = path;
+		this.setExists(true);
+	}
+	public void setTracked(boolean tracked) { this.tracked = tracked; }
+	public void setExists(boolean exists) { this.exists = exists; }
+}
